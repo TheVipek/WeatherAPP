c0e55a3410807574eb352b87b5bab4afaa78879d
Vipek <thevipek0552@gmail.com>
2021-11-27 17:14:40 +0100

    Added detailed view of weather After click on button appears div with detailed information about weather that is divided into four stages of day : morning , day , evening, night

--- views.py
+++ views.py
@@ -116,20 +116,19 @@ def get_weather():
     postal = request.cookies.get('postal')
     weather_key = os.environ.get('weatherapi')
 
-    url="https://api.weatherapi.com/v1/forecast.json?key=%s&q=%s,%s&days=%s&aqi=no&alerts=no"%(weather_key,latitude,longitude,1)
+    url="https://api.weatherapi.com/v1/forecast.json?key=%s&q=%s,%s&days=1&aqi=no&alerts=no"%(weather_key,latitude,longitude)
     response = requests.get(url)
     weather = response.json()
-
-    weather_dataset = {"timezone":"","now":{"last_updated":"","temp_c":"","condition_text":"","condition_icon":""},"current":{'last_updated':"","temp_c":"","condition":{"name":"","icon":""}},
+    weather_dataset = {"timezone":"","now":{"last_updated":"","temp_c":"","condition_text":"","condition_icon":""},
     "days":[]}
 
-    weather_dataset["current"]["last_updated"] = weather["current"]["last_updated"].split(' ')[1]
-    weather_dataset["current"]["temp_c"] = weather["current"]["temp_c"]
-    weather_dataset["current"]["condition"]["name"] = weather["current"]["condition"]["text"]
-    weather_dataset["current"]["condition"]["icon"] = weather["current"]["condition"]["icon"]
+    # weather_dataset["current"]["last_updated"] = weather["current"]["last_updated"].split(' ')[1]
+    # weather_dataset["current"]["temp_c"] = round(weather["current"]["temp_c"])
+    # weather_dataset["current"]["condition"]["name"] = weather["current"]["condition"]["text"]
+    # weather_dataset["current"]["condition"]["icon"] = weather["current"]["condition"]["icon"]
 
     for count,day in enumerate(weather["forecast"]["forecastday"]):
-        info = {"pressure":[],"wind_kph":[],"time":[],"temp_c":[],"condition":{"name":[],"icon":[]}}
+        info = {"pressure":[],"wind_kph":[],"time":[],"temp_c":[],"feelslike_c":[],"condition":{"name":[],"icon":[]},"chance_of_rain":[],"chance_of_snow":[]}
         for x_hour in day['hour']:
             # print(math.ceil(x_hour['pressure_mb']))
             # print(math.ceil(x_hour['wind_kph']))
@@ -137,12 +136,15 @@ def get_weather():
             # print(math.ceil(x_hour['temp_c']))
             # rounded_temp = x_hour['temp_c']
             # {"pressure":[],"wind_kph":[],"time":[],"temp_c":[],"condition":{"name":[],"icon":[]}}
-            info["pressure"].append(math.ceil(x_hour['pressure_mb']))
-            info["wind_kph"].append(math.ceil(x_hour['wind_kph']))
+            info["pressure"].append(round(x_hour['pressure_mb']))
+            info["wind_kph"].append(round(x_hour['wind_kph']))
             info["time"].append(x_hour['time'].split(' ')[1])
-            info["temp_c"].append(math.ceil(x_hour['temp_c']))
+            info["temp_c"].append(round(x_hour['temp_c']))
             info["condition"]["name"].append(x_hour['condition']["text"])
             info["condition"]["icon"].append(x_hour['condition']["icon"])
+            info["chance_of_rain"].append(x_hour["chance_of_rain"])
+            info["chance_of_snow"].append(x_hour["chance_of_snow"])
+            info["feelslike_c"].append(x_hour["feelslike_c"])
         weather_dataset["days"].append(info)
     
     url="https://api.weatherapi.com/v1/current.json?key=%s&q=%s,%s&aqi=no&alerts=no"%(weather_key,latitude,longitude)
@@ -151,8 +153,7 @@ def get_weather():
     weather_dataset["timezone"] = weather["location"]["tz_id"]
     weather_dataset["now"]["last_updated"] = weather["current"]["last_updated"]
     weather_dataset["now"]["last_updated"] = weather_dataset["now"]["last_updated"].split(' ')[1]
-    print(weather_dataset["now"]["last_updated"])
-    weather_dataset["now"]["temp_c"] = weather["current"]["temp_c"]
+    weather_dataset["now"]["temp_c"] = round(weather["current"]["temp_c"])
     weather_dataset["now"]["condition_text"] = weather["current"]["condition"]["text"]
     weather_dataset["now"]["condition_icon"] = weather["current"]["condition"]["icon"]
 
@@ -161,6 +162,7 @@ def get_weather():
     # with open ('static/weather_data.json','w') as to_save:
     #     json.dump(weather_dataset,to_save)
     app.logger.info("Got weather data informations")
+    print(weather_dataset['days'][0]['condition']['icon'][7])
     return weather_dataset
 
 
